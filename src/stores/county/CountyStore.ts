3ea5c5d324154233184401ba9dfc0760dcf479f2
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-25 09:17:04 +0200

    countyselector

--- src/stores/county/CountyStore.ts
+++ src/stores/county/CountyStore.ts
@@ -10,7 +10,10 @@ export default defineStore('countyStore', {
     actions: {
         initializeCountryList() {
             DbMethods.getCountryListFromDb().then((r: Record<string, unknown>) => {
-                this.counties = <CountyType>r;
+                this.counties = [];
+                for (let county of Object.values(r)) {
+                    this.counties.push(<CountyType>county);
+                }
                 this.loaded = true;
             });
         }
