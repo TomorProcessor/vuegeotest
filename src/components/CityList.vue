1618a15f3c6272c2861456e35523dbce5a139590
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-25 17:32:22 +0200

    city modifiers, city deselect

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -2,8 +2,20 @@
 
 import Styles from "@/components/Styles";
 import CountyStore from "@/stores/county/CountyStore";
+import {ref} from "vue";
+import type {Ref, UnwrapRef} from "@vue/reactivity";
+import {cityList} from "@/stores/ApiUrls";
 
 const store = CountyStore();
+const selectedCityId: Ref<UnwrapRef<number>> = ref(NaN);
+
+function selectCity(id: number) {
+  selectedCityId.value = id;
+}
+
+function deselectCity() {
+  selectedCityId.value = NaN;
+}
 
 </script>
 
@@ -13,10 +25,26 @@ const store = CountyStore();
       <h2 class="underline mr-2">Megye:</h2>
       <p>{{ store.selectedCounty?.name }}</p>
     </div>
-    <h2 class="underline">Városok: </h2>
-    <p v-for="city in store.citiesForSelectedCounty">
-      {{ city.name }}
-    </p>
+    <h2 class="underline justify-between items-center">Városok: </h2>
+    <div class="flex" v-for="city in store.citiesForSelectedCounty">
+      <p @click="selectCity(city.id)">
+        {{ city.name }}
+      </p>
+      <div class="flex">
+        <button v-if="selectedCityId == city.id"
+                class="g-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          Törlés
+        </button>
+        <button v-if="selectedCityId == city.id"
+                class="g-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          Módosít
+        </button>
+        <button @click="deselectCity" v-if="selectedCityId == city.id"
+                class="g-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+          Mégsem
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
